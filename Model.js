83d723ed63ebd70879d54b345aa7bb5b7fd38dba
BrightMontain <brightMontain@outlook.com>
2024-12-28 19:31:45 +0300

    Update and rename core.task to Model.js

---
+++ Model.js
@@ -0,0 +1,13 @@
+import data from "./data.js";
+function task()
+{
+  while (true)
+  {
+    var bridge += data('string', '-compilation');
+    if data('string', '-compilation') == 'string-compilation'
+    {
+      break;
+    }
+  }
+  return bridge;
+}
