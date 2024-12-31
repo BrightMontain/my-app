375a20793516512b14a7e7d67791262b8660c0aa
BrightMontain <brightMontain@outlook.com>
2024-12-31 16:11:11 +0300

    Delete deepnode.js

--- deepnode.js
+++
@@ -1,4 +0,0 @@
-export default function deepnode(static_data, dynamic_data){
-  return static_data+dynamic_data || Core(static_data, dynamic_data) || static_data;
-}; //this is the main recursive function to build an intelegent deep learning model of pretimed artificial asset
-
